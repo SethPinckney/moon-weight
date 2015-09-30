9c8cb9abb7e815c92f49e8b985b2dc8cea233e4b
Seth Pinckney <sethpinckney@Seths-MacBook-Pro.local>
2015-09-29 23:18:39 -0400

    9/29/2015 Tech Elevator Cohort [0] Homework

---
+++ MoonWeight.java
@@ -0,0 +1,71 @@
+class MoonWeight {
+	
+public static void main (String args []){
+
+	int planetearthweight = 220;
+	double moonweight = planetearthweight * 0.17;
+
+    System.out.print("My weight on Earth is ");
+	System.out.println(planetearthweight +
+        "lbs which is equivalent to " +
+        moonweight + " moon-pounds.");
+	System.out.println("I'm light on the moon ;-D");
+}
+
+}
+/* Other homework questions are answered in these wonderful comments. Here goes nothing 
+
+5. Which of the following variable names is invalid?
+
+A. count
+B. $count
+C. count27
+D. 67count
+
+the answer is D. - variable names cannot begin with a numeral
+
+6. How do you create a single-line comment? How do you create a multiline comment?
+ */
+
+//This is a single line comment
+
+// If I want to creat multiple lines of comments
+
+/* I need to use this structure */
+
+/* 
+7. Show the general form of the if statement. Show the general form of the for loop. 
+
+if (testisgreaterthan20);
+for (int test=10; test>25; --test);
+
+8. How do you create a block of code?
+
+A code block goes in between curly brackets {
+
+System.out.println("and a statement is followed by a semicolon");
+
+}
+
+9. 
+
+class MoonWeight {
+	
+public static void main (String args []){
+
+	int planetearthweight = 220;
+	double moonweight = planetearthweight * 0.17;
+
+    System.out.print("My weight on Earth is ");
+	System.out.println(planetearthweight +
+        "lbs which is equivalent to " +
+        moonweight + " moon-pounds.");
+	System.out.println("I'm light on the moon ;-D");
+}
+
+}
+
+11. If you made a typing mistake when entering your program, what sort of error will result?
+
+Typos result in the "syntax error" message
+*/
